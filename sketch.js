c46b7001cee09d7300bc2c08cd7dbb826c630843
KH-CC <55528312+KH-CC@users.noreply.github.com>
2019-09-22 15:37:16 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,11 +1,32 @@
 function setup() {
-    createCanvas(windowWidth, windowHeight);
-    background(255,0,0);
+  createCanvas(windowWidth, windowHeight);
+  background(17, 11, 36);
 }
-    
-function draw() {
-    if (mouseIsPressed) {
-        var size = random(10, 100);
-        ellipse(mouseX, mouseY, size, size);
+
+
+function draw() { 
+  noStroke();
+  if (mouseIsPressed) {
+    frameRate(9);
+    fill(random(255));
+    var starr = random(5);
+    ellipse( random(width) , random(height) ,  starr,  starr);
+  }
+  else {
+    if (keyIsPressed)
+    if(key == 'p' || key =='P' ){
+      frameRate(0.5);
+      fill(random(255), random(255), random(255));
+      var planetr = random(20, 30);
+      ellipse( random(width), random(height), planetr, planetr);
     }
+   else {
+     if (keyIsPressed)
+     if(key == 'c' || key =='C' ){
+       frameRate(1);
+       fill(253, 255, 181);
+       ellipse( mouseX, mouseY, 7, 7);
+     }
+   }
+  }
 }
